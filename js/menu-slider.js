33e76153e2db665bf345b3f38ba404290106b0a2
Chris Franks <ChrisFranksTrippy@users.noreply.github.com>
2018-01-05 12:12:26 -0500

    Debugging
    Researched the following solution:
    
    https://stackoverflow.com/questions/3705937/document-click-not-working-correctly-on-iphone-jquery

--- js/menu-slider.js
+++ js/menu-slider.js
@@ -7,21 +7,11 @@
     let menuDraw = document.getElementById("menu-draw");
 
     docBody.addEventListener("click", requestMenuDraw);
-    mBurger.addEventListener("click", testRequest);
 
     function requestMenuDraw(event) {
-        
-        //alert("requestMenuDraw start Working");
-        
-        if (event.target.tagName.toLocaleLowerCase() === "nav" || event.target.id === "menu-burger" || event.target.classList.contains("menu-span")) {
-            console.log("Do nothing.");
-        } else if (menuDraw.classList.contains("show-menu")){
-            console.log("MenuDraw is open anything but the nav container was clicked.");
-            toggleMenuDraw();
-        }
-        
-        /*
+
         if (event.target.id === "menu-burger" || event.target.classList.contains("menu-span")) {
+            
             toggleMenuDraw();
         } else if (event.target.tagName.toLocaleLowerCase() === "nav") {
             console.log("Do nothing, Nav container clicked.");
@@ -29,21 +19,11 @@
             console.log("MenuDraw is open anything but the nav container was clicked.");
             toggleMenuDraw();
         }
-        */
-        
-        //alert("requestMenuDraw end Working");
-    }
-    
-    function testRequest() {
-        toggleMenuDraw();
     }
 
     function toggleMenuDraw() {
         //Prevent user seeing menu transition when changing 
         //the size of the window in real time
-        
-        //alert("toggleMenuDraw start Working");
-        
         menuDraw.classList.add("transition-menu");
 
         requestAnimationFrame(function () {
@@ -59,8 +39,6 @@
 
             menuDraw.addEventListener("transitionend", removeTransition);
         });
-        
-        //alert("toggleMenuDraw end Working");
     }
 
     function removeTransition() {
