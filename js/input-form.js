04e3403e53e73ed8e40bf89440cda1f4c3072ece
Chris Franks <1000frankschris@gmail.com>
2018-01-13 12:03:22 +0000

    Cover layer added
    An overlay on top of the screen to give feed back and interaction on the
    next actions to take within the application

--- js/input-form.js
+++ js/input-form.js
@@ -2,56 +2,69 @@
 
 (function () {
 
-    let uagent = navigator.userAgent.toLowerCase();
-    let docBody = document.getElementsByTagName("body")[0];
-    let ctas = document.getElementsByClassName("cta");
-    let checkboxContainers = document.getElementsByClassName("checkbox-container");
+	let uagent = navigator.userAgent.toLowerCase();
+	let docBody = document.getElementsByTagName("body")[0];
+	let ctas = document.getElementsByClassName("cta");
+	let checkboxContainers = document.getElementsByClassName("checkbox-container");
 
-    if (uagent.search("iphone") > -1 || uagent.search("ipad") > -1) {
-        docBody.style.cursor = "pointer";
-    } else {
-        console.log("Not an iPhone or iPad.");
-    }
+	let coverLayer = document.getElementById("cover-layer");
+	let raiseTicketBtn = document.getElementById("new-ticket");
+	let sendBtn = document.getElementById("submit-button");
 
-    for (let i = 0; i < ctas.length; i++) {
-        ctas[i].addEventListener("click", ctaHandler);
-    }
+	if (uagent.search("iphone") > -1 || uagent.search("ipad") > -1) {
+		docBody.style.cursor = "pointer";
+	} else {
+		console.log("Not an iPhone or iPad.");
+	}
 
-    for (let i = 0; i < checkboxContainers.length; i++) {
-        checkboxContainers[i].addEventListener("click", checkboxHandler);
-    }
+	for (let i = 0; i < ctas.length; i++) {
+		ctas[i].addEventListener("click", ctaHandler);
+	}
 
+	for (let i = 0; i < checkboxContainers.length; i++) {
+		checkboxContainers[i].addEventListener("click", checkboxHandler);
+	}
 
-    
-    function ctaHandler(event) {
-        event.target.classList.add("toggle");        
-        event.target.addEventListener("transitionend", ctaTransitionend);
-    }
-    
-    function ctaTransitionend(event) {
-        event.target.classList.remove("toggle");
-    }
+	sendBtn.addEventListener("click", submitForm);
+	raiseTicketBtn.addEventListener("click", newTicket);
 
-    function checkboxHandler(event) {
+	function ctaHandler(event) {
+		event.target.classList.add("toggle");
+		event.target.addEventListener("transitionend", ctaTransitionend);
+	}
 
-        if (event.target.tagName.toLocaleLowerCase() === "span") {
-            //Get value
-            let innerValue = event.target.innerHTML.toLocaleLowerCase();
-            let yesSpan = event.target.parentElement.children[0];
-            let noSpan = event.target.parentElement.children[1];
+	function ctaTransitionend(event) {
+		event.target.classList.remove("toggle");
+	}
 
-            //Set input value
-            event.target.parentElement.parentElement.children[0].value = innerValue;
+	function checkboxHandler(event) {
 
-            if (innerValue === "yes") {
-                yesSpan.classList.add("toggle");
-                noSpan.classList.remove("toggle");
-            } else {
-                yesSpan.classList.remove("toggle");
-                noSpan.classList.add("toggle");
-            }
+		if (event.target.tagName.toLocaleLowerCase() === "span") {
+			//Get value
+			let innerValue = event.target.innerHTML.toLocaleLowerCase();
+			let yesSpan = event.target.parentElement.children[0];
+			let noSpan = event.target.parentElement.children[1];
 
-        }
-    }
+			//Set input value
+			event.target.parentElement.parentElement.children[0].value = innerValue;
+
+			if (innerValue === "yes") {
+				yesSpan.classList.add("toggle");
+				noSpan.classList.remove("toggle");
+			} else {
+				yesSpan.classList.remove("toggle");
+				noSpan.classList.add("toggle");
+			}
+
+		}
+	}
+
+	function submitForm() {
+		coverLayer.classList.add("flex");
+	}
+	
+	function newTicket() {
+		coverLayer.classList.remove("flex");
+	}
 
 })()
