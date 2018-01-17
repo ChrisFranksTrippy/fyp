21e8fe71a59193b27c9e1808df4adf6224f382fa
Chris Franks <1000frankschris@gmail.com>
2018-01-17 19:11:00 +0000

    Grid Mobile Search Shell
    Converted the footer of the table to a mobile search pop-up.
    Added a mobile user friendly style to the shell
    Added Font Awesome for icons where the colours can easily be changed.
    Added JS to ticket-js to handle the button click of the search tab.

--- js/ticket-js.js
+++ js/ticket-js.js
@@ -5,9 +5,14 @@
 	let lastClickTime = new Date().getTime();
 	let ticketCards = document.getElementById("t-body").getElementsByClassName("cta");
 	
+	let mobileSearchBtn = document.getElementById("mobile-search-button");
+	let mobileSearchContainer = document.getElementById("t-footer");
+	
 	for (let i = 0; i < ticketCards.length; i++) {		
 		ticketCards[i].addEventListener("click", doubleTabRequest);		
 	}
+	
+	mobileSearchBtn.addEventListener("click", toggleMobileSearch);
 	
 	
 	
@@ -23,6 +28,22 @@
 			//console.log(event.target);
 		}
 
+	}
+	
+	function toggleMobileSearch() {		
+		
+		if (mobileSearchContainer.classList.contains("show")){
+			mobileSearchContainer.classList.remove("show");
+			mobileSearchBtn.classList.add("fa-search");
+			mobileSearchBtn.classList.remove("fa-close");
+		} else {
+			mobileSearchContainer.classList.add("show");
+			mobileSearchBtn.classList.remove("fa-search");
+			mobileSearchBtn.classList.add("fa-close");
+		}
+		
+		
+		
 	}
 
 })();
