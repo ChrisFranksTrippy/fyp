53e068e20d39b4dfb83f76e30fb101e44d7c4a67
Chris Franks <1000frankschris@gmail.com>
2018-01-22 20:20:32 +0000

    Final Commit to Prototype
    Finished the edit-form page.
    Slider and date picker added.
    Js to slide check slider added.

--- js/edit-form.js
+++ js/edit-form.js
@@ -2,11 +2,19 @@
 
 (function(){
 	
+	let creditSlider = document.getElementById("credit-recieved-slider");
+	
 	let saveBtn = document.getElementById("save-button");
 	let cancelBtn = document.getElementById("cancel-button");
 	
+	creditSlider.addEventListener("click", sliderHandler);
+	
 	saveBtn.addEventListener("click", saveForm);
 	cancelBtn.addEventListener("click", cancelForm);
+	
+	function sliderHandler() {
+		creditSlider.classList.toggle("slide");
+	}
 	
 	function saveForm() {
 		
